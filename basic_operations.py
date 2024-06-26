cb78a43c0e9f48fd1d7ed8d6012268029ef95ccf
samitech <techanes39@gmail.com>
2024-06-26 11:38:59 +0200

    add basic_operations.py with some code

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+difference = number1 - number2
+result = number1 * number2
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(sum))
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(difference))
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(result))
+
